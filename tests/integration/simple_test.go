2f9632526670e9be7fbb667161d796d1194d5b32
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2025-03-20 23:22:39 +0530

    Add simple_test.go for basic functionality test

---
+++ tests/integration/simple_test.go
@@ -0,0 +1,11 @@
+package integration
+
+import (
+	"testing"
+)
+
+// TestSimple is a very basic test that should always compile
+func TestSimple(t *testing.T) {
+	// This is just a placeholder test to ensure our package compiles
+	t.Log("Simple test running")
+}
