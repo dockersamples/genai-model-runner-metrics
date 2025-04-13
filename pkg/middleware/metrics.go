52ca48d09a62aef898d2dc9ce30523795fbce1f1
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2025-04-14 00:48:22 +0800

    Include responseWriterWrapper definition directly in metrics.go

--- pkg/middleware/metrics.go
+++ pkg/middleware/metrics.go
@@ -8,6 +8,31 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// responseWriterWrapper is a custom response writer that captures the status code
+type responseWriterWrapper struct {
+	w          http.ResponseWriter
+	statusCode int
+}
+
+func (rww *responseWriterWrapper) Header() http.Header {
+	return rww.w.Header()
+}
+
+func (rww *responseWriterWrapper) Write(bytes []byte) (int, error) {
+	return rww.w.Write(bytes)
+}
+
+func (rww *responseWriterWrapper) WriteHeader(statusCode int) {
+	rww.statusCode = statusCode
+	rww.w.WriteHeader(statusCode)
+}
+
+func (rww *responseWriterWrapper) Flush() {
+	if f, ok := rww.w.(http.Flusher); ok {
+		f.Flush()
+	}
+}
+
 // MetricsMiddleware adds Prometheus metrics to HTTP requests
 func MetricsMiddleware(requestCounter *prometheus.CounterVec, requestDuration *prometheus.HistogramVec, activeRequests prometheus.Gauge) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
