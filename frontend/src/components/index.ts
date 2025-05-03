c247a2153422ff768e141d5237e14d33e0b78b80
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2025-05-04 02:14:33 +0530

    Add index.ts to export all components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,8 @@
+// Export all components from a single file for easier imports
+export { default as ChatBox } from './ChatBox';
+export { MessageList } from './MessageList';
+export { MessageInput } from './MessageInput';
+export { SimplifiedMetrics } from './SimplifiedMetrics';
+export { ModelInfoCard } from './ModelInfoCard';
+export { LlamaCppMetricsPanel } from './LlamaCppMetricsPanel';
+export { Header } from './Header';
